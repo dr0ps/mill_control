125056b640ed74be48f5ac402a26219c14e54733
dr0ps <dr0ps@users.noreply.github.com>
2020-12-30 16:10:21 +0100

    First working thread code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use iced::{slider, Column, Element, ProgressBar, Sandbox, Settings, Slider};
 use std::process::exit;
-use crate::tinyg::Tinyg;
-use std::io::{stdout, Write};
+use crate::tinyg::{Tinyg};
+use std::io::{stdout, Write, BufReader, Read};
+
+extern crate gdk;
+extern crate gtk;
+extern crate gio;
+
+use gtk::prelude::*;
+use glib::{clone};
+use std::fs::File;
+
+use std::thread;
+use std::time;
 
 mod tinyg;
+
+enum Message {
+    UpdateLabel(tinyg::Status),
+}
 
 pub fn main() {
     let mut tinyg = Tinyg::new();
@@ -38,49 +52,92 @@ pub fn main() {
         }
     }
 
-    Progress::run(Settings::default())
-}
-
-#[derive(Default)]
-struct Progress {
-    value: f32,
-    progress_bar_slider: slider::State,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    SliderChanged(f32),
-}
-
-impl Sandbox for Progress {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self::default()
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
     }
 
-    fn title(&self) -> String {
-        String::from("Hallo!")
-    }
+    let style = include_str!("../style.css");
+    let provider = gtk::CssProvider::new();
+    provider
+        .load_from_data(style.as_bytes())
+        .expect("Failed to load CSS");
+    // We give the CssProvided to the default screen so the CSS rules we added
+    // can be applied to our window.
+    gtk::StyleContext::add_provider_for_screen(
+        &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::SliderChanged(x) => self.value = x,
+    let glade_src = include_str!("../mainwindow.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    let main_window: gtk::Window = builder.get_object("main_window").unwrap();
+    let text_view: gtk::TextView = builder.get_object("gcode_view").unwrap();
+
+    let file_choose_button : gtk::FileChooserButton = builder.get_object("file_choose_button").unwrap();
+    file_choose_button.connect_file_set(clone!(@weak text_view => move |file_choose_button| {
+        let filename = file_choose_button.get_filename().expect("Couldn't get filename");
+        let file = File::open(&filename).expect("Couldn't open file");
+
+        let mut reader = BufReader::new(file);
+        let mut contents = String::new();
+        let _ = reader.read_to_string(&mut contents);
+
+        text_view
+            .get_buffer()
+            .expect("Couldn't get window")
+            .set_text(&contents);
+
+    }));
+
+    let pos_x: gtk::Label = builder.get_object("pos_x").unwrap();
+    let pos_y: gtk::Label = builder.get_object("pos_y").unwrap();
+    let pos_z: gtk::Label = builder.get_object("pos_z").unwrap();
+    let pos_a: gtk::Label = builder.get_object("pos_a").unwrap();
+
+    let (sender, receiver) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let button : gtk::Button = builder.get_object("refallhome_button").unwrap();
+    button.connect_clicked(clone!(@strong tinyg => move |_x| { tinyg.clone().home_all(); }));
+
+    thread::spawn(move || {
+        loop {
+            thread::sleep(time::Duration::from_millis(100));
+            // Sending fails if the receiver is closed
+            let status= tinyg.get_latest_status().unwrap();
+            let _ = sender.send(Message::UpdateLabel(status));
         }
-    }
+    });
 
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
-            .padding(20)
-            .push(ProgressBar::new(0.0..=100.0, self.value))
-            .push(
-                Slider::new(
-                    &mut self.progress_bar_slider,
-                    0.0..=100.0,
-                    self.value,
-                    Message::SliderChanged,
-                ),
-            )
-            .into()
-    }
+// Attach the receiver to the default main context (None)
+// and on every message update the label accordingly.
+    let pos_x_clone = pos_x.clone();
+    let pos_y_clone = pos_y.clone();
+    let pos_z_clone = pos_z.clone();
+    let pos_a_clone = pos_a.clone();
+    receiver.attach(None, move |msg| {
+        match msg {
+            Message::UpdateLabel(status) => {
+                pos_x_clone.set_text(format!("{:.4}", status.posx).as_str());
+                pos_y_clone.set_text(format!("{:.4}", status.posy).as_str());
+                pos_z_clone.set_text(format!("{:.4}", status.posz).as_str());
+                pos_a_clone.set_text(format!("{:.4}", status.posa).as_str());
+            },
+        }
+        // Returning false here would close the receiver
+        // and have senders fail
+        glib::Continue(true)
+    });
+
+    main_window.connect_delete_event(|_, _| {
+        // Stop the main loop.
+        gtk::main_quit();
+        // Let the default handler destroy the window.
+        Inhibit(false)
+    });
+
+    main_window.show_all();
+
+    gtk::main();
 }
--- src/tinyg.rs
+++ src/tinyg.rs
@@ -3,9 +3,31 @@ use std::time::{Duration, Instant};
 use std::io::{stdout, Write};
 use std::ops::Add;
 use serde::{Deserialize, Serialize};
+use std::thread;
+use lazy_static::lazy_static;
+use std::sync::{Mutex};
+use std::rc::{Weak};
+use glib::clone::Downgrade;
+
+lazy_static! {
+    static ref LINES_READ : Mutex<Vec<String>> = Mutex::new(vec![]);
+    static ref STATUS : Mutex<Status> = Mutex::new(Status {
+            posx: 0.0,
+            posy: 0.0,
+            posz: 0.0,
+            posa: 0.0,
+            feed: 0.0,
+            vel: 0.0,
+            unit: 0,
+            coor: 0,
+            dist: 0,
+            frmo: 0,
+            stat: 0
+        });
+}
 
 pub struct Tinyg {
-    port : Option<Box<dyn SerialPort>>
+    port : Option<Box<dyn SerialPort>>,
 }
 
 #[derive(Serialize)]
@@ -13,7 +35,7 @@ struct SetVerbosity {
     sv: u16,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone, Copy)]
 pub struct Status {
     #[serde(default)] pub posx: f32,
     #[serde(default)] pub posy: f32,
@@ -41,8 +63,9 @@ struct StatusReportResult {
 
 fn send( port: &mut Box<dyn SerialPort>, message: &str) -> Result<String, String>
 {
-    println!("Sending >{}<", message);
-    stdout().flush().unwrap();
+    println!("Sending {}", message);
+    let json_only = message.starts_with('{');
+
     match port.write(message.as_bytes())
     {
         Ok(_size) => {
@@ -61,58 +84,46 @@ fn send( port: &mut Box<dyn SerialPort>, message: &str) -> Result<String, String
             return Err(err.to_string());
         }
     }
-    let mut result = String::new();
 
-    let mut start = Instant::now();
 
-    loop {
-        match port.bytes_to_read() {
-            Ok(bytes) => {
-                if bytes == 0 {
-                    if start.elapsed().as_millis() > 100
-                    {
-                        break;
-                    }
-                }
-                else {
-                    let mut buffer = [0u8; 4096];
-                    match port.read(&mut buffer)
-                    {
-                        Ok(size) => {
-                            result = result.add(String::from_utf8_lossy(&buffer[0..size]).trim());
-                        }
-                        Err(err) => {
-                            return Err(err.to_string());
-                        }
-                    }
-                    start = Instant::now();
-                }
-            }
-            Err(err) => {
-                return Err(err.to_string());
-            }
-        }
-    }
+    let start = Instant::now();
+
     let mut final_result= None;
-        for line in result.lines() {
-            println!("Line: >{}<", line);
+    loop {
+        let mut lines = LINES_READ.lock().expect("blah!");
+        if !lines.is_empty() {
+            let prefix : Vec<String> = lines.drain(0..1).collect();
+            let line = prefix.first().expect("Vector was not empty.");
             if line.starts_with("{\"r\":") {
                 final_result = Some(String::from(line));
             }
-            else if line.starts_with("{\"sr\":") {
-                let status: StatusReport = serde_json::from_str(line).unwrap();
-            }
-            else if (line.starts_with("tinyg [mm] ok>"))
+            else if !json_only && line.starts_with("tinyg [mm] ok>")
             {
                 final_result = Some(String::from(line));
             }
+            if final_result.is_some() {
+                break;
+            }
+        } else {
+            if start.elapsed().as_millis() > 1000
+            {
+                return Err(String::from("Timeout."));
+            }
         }
+    }
+
     return Ok(final_result.expect("Has to be here!"));
 }
 
 impl Tinyg {
     pub fn new() -> Self {
         Self { port:None }
+    }
+
+    pub fn get_latest_status(&mut self) -> Result<Status, String>
+    {
+        let status = *STATUS.lock().expect("blah!");
+        return Ok(status);
     }
 
     pub fn initialize(&mut self) -> Result<(), String> {
@@ -147,10 +158,100 @@ impl Tinyg {
         };
         let mut port = serialport::open_with_settings(tinyg_port, &s).expect("Failed to open serial port");
 
+        {
+            let mut port_clone = port.try_clone().expect("Has to be able to clone");
+            thread::spawn(move || {
+                let mut result = String::new();
+                loop {
+                    let mut buffer = [0u8; 4096];
+                    match port_clone.read(&mut buffer)
+                    {
+                        Ok(size) => {
+                            result = result.add(String::from_utf8_lossy(&buffer[0..size]).trim());
+                            println!("Current buffer length: {}", result.len());
+                            let mut start : i32 = -1;
+                            let mut chars = result.char_indices();
+                            let mut char_at = chars.next();
+                            'first_brace: while char_at.is_some() {
+                                if char_at.unwrap().1 == '{' {
+                                    start = char_at.unwrap().0 as i32;
+                                    break 'first_brace;
+                                }
+                                char_at = chars.next();
+                            }
+                            if start == -1
+                            {
+                                let mut chars = result.char_indices();
+                                let mut char_at = chars.next();
+                                while char_at.is_some() {
+                                    if char_at.unwrap().1 == '\n' {
+                                        let line  = String::from(result.clone()[0..char_at.unwrap().0].trim());
+                                        LINES_READ.lock().expect("blah!").push(line);
+                                        result = String::from(result.split_off(char_at.unwrap().0+1));
+                                        chars = result.char_indices();
+                                    }
+                                    char_at = chars.next();
+                                }
+                            }
+                            else {
+                                let line  = String::from(&result.clone()[0..start as usize]);
+                                if line.trim().len() > 0
+                                {
+                                    LINES_READ.lock().expect("blah!").push(String::from(line.trim()));
+                                }
+                                result = String::from(result.split_off(start as usize).trim());
+
+                                'json: while result.len() > 0 {
+                                    let mut end = 0;
+                                    let mut open_brace_count = 1;
+                                    let mut chars = result.char_indices();
+                                    'brace_count: while open_brace_count > 0 {
+                                        let mut char_at = chars.next();
+                                        if char_at.is_some() {
+                                            if char_at.unwrap().1 == '}' {
+                                                open_brace_count = open_brace_count - 1;
+                                                if open_brace_count == 0
+                                                {
+                                                    end = char_at.unwrap().0 + 1;
+                                                }
+                                            }
+                                            if char_at.unwrap().1 == '{' && char_at.unwrap().0 > 1 {
+                                                open_brace_count = open_brace_count + 1;
+                                            }
+                                        } else {
+                                            break 'brace_count; //incomplete
+                                        }
+                                    }
+                                    if open_brace_count == 0 {
+                                        let sub = &result.clone()[0..end];
+                                        let sub = sub.trim();
+                                        result = String::from(result.split_off(end));
+
+                                        if sub.starts_with("{\"sr\":") {
+                                            let status: StatusReport = serde_json::from_str(sub).expect(format!("Unable to run serde with this input: >{}<", sub).as_str());
+                                            *STATUS.lock().expect("blah!") = status.sr.clone();
+                                        }
+                                        else {
+                                            println!("Passing {}", sub);
+                                            LINES_READ.lock().expect("blah!").push(String::from(sub));
+                                        }
+                                    } else {
+                                        break 'json;
+                                    }
+                                }
+                            }
+                        }
+                        Err(_err) => {}
+                    }
+                }
+            });
+        }
+
         let mut initialized= false;
         for _x in 0..10 {
             match send(&mut port, "$\r\n") {
                 Ok(result) => {
+                    println!("result: {}",result);
                     if result.trim().contains(&String::from("tinyg [mm] ok>"))
                     {
                         println!("Init received {}", result);
@@ -203,7 +304,29 @@ impl Tinyg {
         let verbosity = SetVerbosity{sv:2};
         let _result = send(self.port.as_mut().expect(""), serde_json::to_string(&verbosity).unwrap().add("\r\n").as_str())?;
         let result = send(self.port.as_mut().expect(""), "{\"sr\":null}\r\n")?;
+        println!("Parsing >{}<", result);
         let status_report: StatusReportResult = serde_json::from_str(result.as_str()).unwrap();
         Ok(status_report.r)
+    }
+
+    pub fn home_all(&mut self) -> Result<String, String> {
+        let result = send(self.port.as_mut().expect(""), "{\"gc\":\"g28.2 x0 y0 z0\"}\r\n")?;
+        Ok(result)
+    }
+}
+
+impl Clone for Tinyg {
+    fn clone(&self) -> Self {
+        let port = self.port.as_ref().unwrap().try_clone().unwrap();
+        return Self {port : Option::Some(port)};
+    }
+}
+
+impl Downgrade for Tinyg {
+
+    type Weak = Weak<Tinyg>;
+
+    fn downgrade(&self) -> Weak<Tinyg> {
+        Weak::new()
     }
 }
